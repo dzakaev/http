28b5f0ad6b917ed726444da1b7350632e73de446
usman <dzakaev.94@mail.ru>
2021-04-29 13:32:08 +0300

    zadacha2.1

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require('http')
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
+const server =http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-type': 'text/html'
+  })
+  res.write('<h1>Hello from intocode coding bootcamp</h1>')
 
-  res.write('ма шаа Аллох1, мял дик г1улкх хили')
+  res.end()
 
-  res.end();
 })
 
-server.listen(3000);
+server.listen(3000)
