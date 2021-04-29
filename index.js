65f7f0382a6828c51f8621258b2b40423bbf9173
usman <dzakaev.94@mail.ru>
2021-04-29 14:56:58 +0300

    save

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
-const server =http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-type': 'text/html'
-  })
-  res.write('<h1>Hello from intocode coding bootcamp</h1>')
-
-  res.end()
-
+const server = http.createServer((req, res) => {
+  res.writeHead(200)
+  if (req.url === '/') {
+    fs.readFile(path.resolve(__dirname, 'pages', 'index.html'),(err, data) => {
+      res.write(data.toString())
+      res.end()
+    })
+  } else if (req.url === '/about') {
+    fs.readFile(path.resolve(__dirname, 'pages', 'about.html'), (err, data) => {
+      res.write(data.toString())
+      res.end()
+    })
+  } else {
+    res.writeHead('404')
+    res.write('страница боь1')
+    res.end()
+  }
 })
-
 server.listen(3000)
